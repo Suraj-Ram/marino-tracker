4755a5c959aad8d49e59d3fb69708a7c52a0b689
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2022-12-19 10:07:14 +0400

    Created live count displays

---
+++ src/PlaceholderData.js
@@ -0,0 +1,35 @@
+const liveCounts = [
+    {
+        key: 0,
+        name: "Marino 2nd Floor",
+        count: 12,
+        subtext: "34%"
+    },
+    {
+        key: 1,
+        name: "Marino Gymnasium",
+        count: 12,
+        subtext: "34%"
+    },
+    {
+        key: 2,
+        name: "Marino Weight Room",
+        count: 12,
+        subtext: "34%"
+    },
+    {
+        key: 3,
+        name: "Marino Cardio",
+        count: 12,
+        subtext: "34%"
+    },
+    {
+        key: 4,
+        name: "Marino Track",
+        count: 12,
+        subtext: "34%"
+    },
+
+]
+
+export {liveCounts}
---
+++ src/components/CountDisplay.jsx
@@ -0,0 +1,25 @@
+import { Paper } from "@mui/material";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+
+function CountDisplay({
+    name,
+    count,
+    subtext
+}) {
+  return (
+    <Paper sx={{p: 2}}>
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        {name}
+      </Typography>
+      <Typography component="p" variant="h4">
+        {count}
+      </Typography>
+      <Typography color="text.secondary" sx={{ flex: 1 }}>
+        {subtext}
+      </Typography>
+    </Paper>
+  );
+}
+
+export default CountDisplay;
