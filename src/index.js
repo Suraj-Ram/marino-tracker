c65beeec53f1f786a79b05ded0d8c1afa07d4622
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2022-12-19 21:53:09 +0400

    tried routing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>pls redirect to marino</div>
+  },
+  {
+    path: '/marino',
+    element: <div>pls redirect to today</div>
+  },
+  {
+    path: '/marino/today',
+    element: <div>marino today view</div>
+  },
+  {
+    path: '/marino/historical',
+    element: <div>marino hist view</div>
+  },
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
