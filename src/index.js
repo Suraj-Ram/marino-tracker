15aeccc64936b9bfe5cd5159796a146376ef525c
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2022-12-20 15:55:13 +0400

    Initial BrowserRouter configuration

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import {
   BrowserRouter,
-  createBrowserRouter,
+  Outlet,
   Route,
-  RouterProvider,
   Routes,
 } from "react-router-dom";
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>pls redirect to marino</div>
-  },
-  {
-    path: '/marino',
-    element: <div>pls redirect to today</div>
-  },
-  {
-    path: '/marino/today',
-    element: <div>marino today view</div>
-  },
-  {
-    path: '/marino/historical',
-    element: <div>marino hist view</div>
-  },
-
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router} />
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/marino" element={<>Marino stub <Outlet/> </>}>
+        <Route path="today" element={"marino today view"} />
+        <Route path="historical" element={"marino hist view"} />
+      </Route>
+      <Route path="/squash" element={<>Squash stub <Outlet/> </>}>
+        <Route path="today" element={"squash today view"} />
+        <Route path="historical" element={"squash hist view"} />
+      </Route>
+
+    </Routes>
+    
+    </BrowserRouter>
+
+
+
+
   </React.StrictMode>
 );
